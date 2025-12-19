e89b9948da62bae7d0d7b918af55f05bb7ab69c5
Jinit Mehta <jinitmehta21@gmail.com>
2025-12-19 21:35:31 +0530

    Added

--- public/index.php
+++ public/index.php
@@ -2,41 +2,34 @@
 
 /**
  * CodeIgniter 4 Front Controller
- * Sparsh Collection - Production
  */
 
-// Minimum PHP version check
 $minPhpVersion = '8.1';
 if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
-    die("PHP {$minPhpVersion} or higher is required. Current: " . PHP_VERSION);
+    die("PHP {$minPhpVersion}+ required. Current: " . PHP_VERSION);
 }
 
-// Path to front controller
+// Path constants
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
-// Ensure the current directory is pointing to the front controller's directory
-if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
-    chdir(FCPATH);
-}
+// Change to front controller directory
+chdir(FCPATH);
 
-// Load paths config
-$pathsConfig = FCPATH . '../app/Config/Paths.php';
-require realpath($pathsConfig) ?: $pathsConfig;
+// Load Paths config
+require FCPATH . '../app/Config/Paths.php';
 
 $paths = new Config\Paths();
 
-// Location of the framework bootstrap file
-$bootstrap = rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
-require realpath($bootstrap) ?: $bootstrap;
+// Load framework bootstrap
+require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
-// Load .env file if exists
-$envFile = FCPATH . '../.env';
-if (is_file($envFile)) {
+// Load .env if exists
+if (is_file(FCPATH . '../.env')) {
     $dotenv = new \CodeIgniter\Config\DotEnv(FCPATH . '../');
     $dotenv->load();
 }
 
-// Launch CodeIgniter
+// Run application
 $app = Config\Services::codeigniter();
 $app->initialize();
 $app->setContext('web');
---
+++ public/test.php
@@ -0,0 +1,5 @@
+<?php
+echo "Server is working!";
+echo "<br>PHP Version: " . PHP_VERSION;
+echo "<br>Document Root: " . $_SERVER['DOCUMENT_ROOT'];
+echo "<br>Script: " . $_SERVER['SCRIPT_FILENAME'];
