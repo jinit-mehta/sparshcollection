a526d1b610002b8fda1a4b737e0972df83b92cc0
Jinit Mehta <jinitmehta21@gmail.com>
2025-12-19 21:02:07 +0530

    Database updated

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -6,56 +6,54 @@ use CodeIgniter\Database\Config;
 
 class Database extends Config
 {
-    public string $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
     public string $defaultGroup = 'default';
     
     public array $default = [
-        'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => false,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_unicode_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'numberConnect'=> null,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
+        'DSN'      => '',
+        'hostname' => '',
+        'username' => '',
+        'password' => '',
+        'database' => '',
+        'DBDriver' => 'MySQLi',
+        'DBPrefix' => '',
+        'pConnect' => false,
+        'DBDebug'  => false,
+        'charset'  => 'utf8mb4',
+        'DBCollat' => 'utf8mb4_unicode_ci',
+        'swapPre'  => '',
+        'encrypt'  => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+        'port'     => 3306,
     ];
 
     public function __construct()
     {
         parent::__construct();
         
-        // Load Railway MySQL variables
-        $this->default['hostname'] = getenv('MYSQLHOST') 
-            ?: ($_ENV['MYSQLHOST'] ?? 'localhost');
-        
-        $this->default['database'] = getenv('MYSQLDATABASE') 
-            ?: ($_ENV['MYSQLDATABASE'] ?? 'sparshcollection');
-        
-        $this->default['username'] = getenv('MYSQLUSER') 
-            ?: ($_ENV['MYSQLUSER'] ?? 'root');
-        
-        $this->default['password'] = getenv('MYSQLPASSWORD') 
-            ?: ($_ENV['MYSQLPASSWORD'] ?? '');
-        
-        $this->default['port'] = (int)(getenv('MYSQLPORT') 
-            ?: ($_ENV['MYSQLPORT'] ?? 3306));
-        
-        // Enable debug in development only
-        $this->default['DBDebug'] = (ENVIRONMENT !== 'production');
+        // Railway MySQL Connection
+        $this->default['hostname'] = getenv('database.default.hostname') 
+            ?: getenv('MYSQLHOST') 
+            ?: 'mysql.railway.internal';
+            
+        $this->default['database'] = getenv('database.default.database') 
+            ?: getenv('MYSQLDATABASE') 
+            ?: 'railway';
+            
+        $this->default['username'] = getenv('database.default.username') 
+            ?: getenv('MYSQLUSER') 
+            ?: 'root';
+            
+        $this->default['password'] = getenv('database.default.password') 
+            ?: getenv('MYSQLPASSWORD') 
+            ?: '';
+            
+        $this->default['port'] = (int)(getenv('database.default.port') 
+            ?: getenv('MYSQLPORT') 
+            ?: 3306);
+            
+        $this->default['DBDriver'] = getenv('database.default.DBDriver') 
+            ?: 'MySQLi';
     }
 }
